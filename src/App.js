dd6956092a176e90eeee518e23d8c5adc37853b1
Abhishek Soni <soni.abhishek@velocis.in>
2025-11-12 20:22:15 +0530

    manage code

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import FetchApi from './Components/FetchApi'
 import GetAxios from './Components/GetAxios';
+import PostAxios from './Components/PostAxios';
 
 function App() {
   return (
     <div className="App">
       {/* <FetchApi/> */}
-      <GetAxios />
+      {/* <GetAxios /> */}
+      <PostAxios />
     </div>
   );
 }
---
+++ src/Components/PostAxios.js
@@ -0,0 +1,43 @@
+import React,{useEffect, useState} from "react";
+import Axios from "axios";
+
+function PostAxios(){
+
+    const [my_title,setTitle] = useState("");
+    const [my_body,setBody] = useState("");
+
+    const handleSubmit = (e) => {
+         e.preventDefault();
+
+        Axios.post('https://jsonplaceholder.typicode.com/posts',{title:my_title,body:my_body}).then((res)=>{
+             console.log(res);
+
+        }).catch((error)=>{
+            console.log(error)
+        })
+
+    }
+    
+    const handlerTitle = (e) => {
+     setTitle(e.target.value)
+    }
+
+    const handlerBody = (e) => {
+        setBody(e.target.value)
+    }
+
+    return(<>
+            <form onSubmit={handleSubmit}>
+            <label>Enter Title:</label>
+            <input type="text" name="title" value={my_title} onChange={handlerTitle}></input>
+            <br/>
+            <label>Enter Body:</label>
+            <input type="text" name="body" value={my_body} onChange={handlerBody}></input>
+            <br/>
+             <button type="submit">Submit</button>
+            </form>
+           </>);
+
+}
+
+export default PostAxios;
